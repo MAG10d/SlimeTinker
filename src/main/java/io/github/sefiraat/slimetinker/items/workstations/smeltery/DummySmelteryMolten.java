45ca70c5ab55b8d1c6b6dbd504957cacb6051305
MAGd_ <109735203+MAG10d@users.noreply.github.com>
2023-06-29 14:52:53 +0800

    Update DummySmelteryMolten.java

--- src/main/java/io/github/sefiraat/slimetinker/items/workstations/smeltery/DummySmelteryMolten.java
+++ src/main/java/io/github/sefiraat/slimetinker/items/workstations/smeltery/DummySmelteryMolten.java
@@ -13,10 +13,10 @@ public final class DummySmelteryMolten {
         "DUMMY_TINKERS_SMELTERY_MOLTEN",
         Material.CHISELED_POLISHED_BLACKSTONE,
         ThemeItemType.MACHINE,
-        "匠魂冶炼炉",
-        "熔融金属是将对应的金属物品",
-        "放入冶炼炉中熔化得到的产物",
-        "需要有岩浆才能熔化金属"
+        "匠魂冶煉爐",
+        "熔融金屬是將對應的金屬物品",
+        "放入冶煉爐中熔化得到的產物",
+        "需要有岩漿才能熔化金屬"
     );
     public static final RecipeType TYPE = new RecipeType(Keys.WS_DUMMY_SMELTERY_M, STACK);
 
